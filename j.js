d7d5d32b84cb96ae3033db66d399bafee11ee1fc
Saiful Khan <mohammadsaifulkhan9829@gmail.com>
2020-07-19 13:56:54 +0530

    1st page

---
+++ j.js
@@ -0,0 +1,9 @@
+function a() {
+    document.getElementById("demo").src="right.jpeg";
+    alert("YOU HAVE TO PUT MASK");
+    document.getElementById("ex").style.visibility = "visible";
+    var p = document.getElementById("body").innerHTML;
+    document.getElementById("body").innerHTML = p.toUpperCase();
+
+
+}
